f8e948dc8167352e8e4f4566d0266b21d45f45fd
Jules Dupuis <jules.dupuis51@gmail.com>
2020-04-30 15:58:16 +0200

    avancement component JulDup Component #3

--- src/app/jul-dup/jul-dup.component.ts
+++ src/app/jul-dup/jul-dup.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jul-dup.component.scss']
 })
 export class JulDupComponent implements OnInit {
-
+  aff = false;
+  listImage: string[] = ['https://i.ytimg.com/vi/ZUsywvbRuIg/maxresdefault.jpg', 'https://medias.pylones.com/6533-thickbox_default/chaussettes-duchesse.jpg']
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  afficher() {
+    this.aff = true;
+  }
+  effacer() { this.aff = false; }
 }
